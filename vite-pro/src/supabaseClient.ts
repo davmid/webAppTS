8cba71ef2c3c3b4a838f090fc02c4c6a0351eaff
dav_mida <81699460+davmid@users.noreply.github.com>
2025-06-20 23:02:41 +0200

    Update

--- vite-pro/src/supabaseClient.ts
+++ vite-pro/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://hlivjxwuxqujhoqwjriu.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsaXZqeHd1eHF1amhvcXdqcml1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzNzA5NjgsImV4cCI6MjA2NTk0Njk2OH0.k4moj7wrKnjzNdy2OPXHdN5IAowJL9414HsgjR8Lkgg'
+const supabaseUrl = 'SUPABASE_URL'
+const supabaseKey = 'SUPABASE_KEY'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
